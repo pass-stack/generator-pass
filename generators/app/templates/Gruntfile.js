522960d951ff27ceaaac290a386da4ed8e9effb6
Amine Hakkou <amine.hakkou.pro@gmail.com>
2016-10-27 00:26:33 +0100

    wiring play sub-project

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -1,3 +1,7 @@
+var fs = require('fs')
+  , path = require('path')
+  , _ = require('lodash');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -15,5 +19,25 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-wiredep');
 
-  grunt.registerTask('default', ['wiredep']);
+  grunt.registerTask('wire-play-modules', function(){
+    var modules = getDirectories(__dirname + '/modules');
+    var playModules = _.map(modules, function(module){
+      return 'lazy val ' + module + ' = (project in file("modules/' + module + '")).enablePlugins(PlayScala)';
+    }).join('\n');
+    var projectDependencies = _.map(modules, function(module){
+      return '.enablePlugins(PlayScala).dependsOn(' + module + ').aggregate(' + module + ')';
+    }).join('\n');
+    var data = fs.readFileSync(path.join(__dirname, 'build.sbt'), 'utf-8')
+      .replace(/\/\/PASS#Modules#Start[\s\S]*\/\/PASS#Modules#End/, '//PASS#Modules#Start\n' + playModules + '\n//PASS#Modules#End\n')
+      .replace(/\/\/PASS#ModuleDependecies#Start[\s\S]*\/\/PASS#ModuleDependecies#End/, '//PASS#ModuleDependecies#Start\n' + projectDependencies + '\n//PASS#ModuleDependecies#End\n');
+    fs.writeFileSync(path.join(__dirname, 'build.sbt'), data, 'utf-8');
+  });
+
+  grunt.registerTask('default', ['wire-play-modules', 'wiredep']);
+
+  function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+      return fs.statSync(path+'/'+file).isDirectory();
+    });
+  }
 };
--- generators/app/templates/build.sbt
+++ generators/app/templates/build.sbt
@@ -2,14 +2,12 @@ name := """<%= appName %>"""
 
 Config.settings
 
-//PASS/Modules/Start
-//lazy val users = (project in file("modules/users")).enablePlugins(PlayScala)
-//PASS/Modules/End
+//PASS#Modules#Start
+//PASS#Modules#End
 
 lazy val main = (project in file(".")).enablePlugins(PlayScala)
-//PASS/ModuleDependecies/Start
-//    .enablePlugins(PlayScala).dependsOn(users).aggregate(users)
-//PASS/ModuleDependecies/End
+//PASS#ModuleDependecies#Start
+//PASS#ModuleDependecies#End
 
 libraryDependencies ++= Config.dependencies
 
