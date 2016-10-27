6fbcad65f3402ebbcda10922dc9a38ff4e6b3388
Amine Hakkou <amine.hakkou.pro@gmail.com>
2016-10-27 01:08:40 +0100

    handling the case when no module is in

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -33,17 +33,21 @@ module.exports = function(grunt) {
 
   grunt.registerTask('wire-play-modules', function(){
     grunt.log.writeln('Wiring play subProjects');
-    var modules = getDirectories(__dirname + '/modules');
-    var playModules = _.map(modules, function(module){
-      return 'lazy val ' + module + ' = (project in file("modules/' + module + '")).enablePlugins(PlayScala)';
-    }).join('\n');
-    var projectDependencies = _.map(modules, function(module){
-      return '.enablePlugins(PlayScala).dependsOn(' + module + ').aggregate(' + module + ')';
-    }).join('\n');
-    var data = fs.readFileSync(path.join(__dirname, 'build.sbt'), 'utf-8')
-      .replace(/\/\/PASS#Modules#Start[\s\S]*\/\/PASS#Modules#End/, '//PASS#Modules#Start\n' + playModules + '\n//PASS#Modules#End\n')
-      .replace(/\/\/PASS#ModuleDependecies#Start[\s\S]*\/\/PASS#ModuleDependecies#End/, '//PASS#ModuleDependecies#Start\n' + projectDependencies + '\n//PASS#ModuleDependecies#End\n');
-    fs.writeFileSync(path.join(__dirname, 'build.sbt'), data, 'utf-8');
+    try{
+      var modules = getDirectories(__dirname + '/modules');
+      var playModules = _.map(modules, function(module){
+        return 'lazy val ' + module + ' = (project in file("modules/' + module + '")).enablePlugins(PlayScala)';
+      }).join('\n');
+      var projectDependencies = _.map(modules, function(module){
+        return '.enablePlugins(PlayScala).dependsOn(' + module + ').aggregate(' + module + ')';
+      }).join('\n');
+      var data = fs.readFileSync(path.join(__dirname, 'build.sbt'), 'utf-8')
+        .replace(/\/\/PASS#Modules#Start[\s\S]*\/\/PASS#Modules#End/, '//PASS#Modules#Start\n' + playModules + '\n//PASS#Modules#End\n')
+        .replace(/\/\/PASS#ModuleDependecies#Start[\s\S]*\/\/PASS#ModuleDependecies#End/, '//PASS#ModuleDependecies#Start\n' + projectDependencies + '\n//PASS#ModuleDependecies#End\n');
+      fs.writeFileSync(path.join(__dirname, 'build.sbt'), data, 'utf-8');
+    }catch(e){
+      grunt.log.writeln(e);
+    }
   });
 
   grunt.registerTask('default', ['wire-play-modules', 'wiredep', 'includeSource']);
