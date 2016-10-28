ba71479414d7c0dac993c61053bbe3b81b67e595
Amine Hakkou <amine.hakkou.pro@gmail.com>
2016-10-28 01:40:21 +0100

    improved ui & ux for module.list

--- generators/module/templates/angular_module/controllers/module.list.controller.js
+++ generators/module/templates/angular_module/controllers/module.list.controller.js
@@ -4,11 +4,39 @@
   angular.module('app.<%= moduleName %>')
   .controller('<%= moduleNameCap %>ListController', <%= moduleNameCap %>ListController);
 
-  <%= moduleNameCap %>ListController.$inject = ['$scope', '<%= moduleNameCap %>Service'];
+  <%= moduleNameCap %>ListController.$inject = ['$scope', '<%= moduleNameCap %>Service', '$mdToast'];
 
-  function <%= moduleNameCap %>ListController($scope, <%= moduleNameCap %>Service){
+  function <%= moduleNameCap %>ListController($scope, <%= moduleNameCap %>Service, $mdToast){
     var vm = this;
     $scope.selected = [];
+
+    function makeToast(content, mdClass){
+      return $mdToast.simple()
+      .textContent(content)
+      .toastClass(mdClass)
+      .position('bottom right')
+      .hideDelay(3000)
+      .action('Close');
+    }
+
+    $scope.$watch('selected', function(newVal){
+      if(newVal.length > 0){
+        $scope.areSelected = true;
+      }else{
+        $scope.areSelected = false;
+      }
+    });
+
+    vm.deleteSelected = function(){
+      var promises = _.map($scope.<%= moduleName %>s, function(<%= moduleName %>){
+        return <%= moduleName %>.$delete.$promise;
+      });
+      Promise.all(promises).then(function(){
+        $mdToast.show(makeToast('<%= moduleName %>s were deleted', 'md-toast-blue'));
+      },function(){
+        $mdToast.show(makeToast('<%= moduleName %>s were not deleted', 'md-toast-red'));
+      });
+    };
 
     $scope.query = {
       limit: 5,
